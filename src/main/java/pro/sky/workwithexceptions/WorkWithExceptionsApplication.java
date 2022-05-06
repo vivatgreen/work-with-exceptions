ba8796accd721b8e502aed82c935dbd781fb1756
vivatgreen <grindsasha@bk.ru>
2022-05-06 13:33:29 +0300

    Работа с исключениями

---
+++ src/main/java/pro/sky/workwithexceptions/WorkWithExceptionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.workwithexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkWithExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkWithExceptionsApplication.class, args);
+	}
+
+}
